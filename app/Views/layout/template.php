d67ec58731cc95d9b65f40fba15b1afc2a6dc754
M. Farhan Fadhillah <fadhillah28.mff@gmail.com>
2022-10-25 19:04:14 +0700

    Bug

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -7,9 +7,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
   <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://getbootstrap.com/docs/5.2/assets/css/docs.css">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css">
-  <title>Hello, world!</title>
+  <title>Home</title>
 </head>
 
 <body class="p-3 m-0 border-0 bd-example">
