ff777a038c782841e97a062facbfd37b5735599b
syahrilfajripratama <syahril.fajripratama2017@students.unila.ac.id>
2022-11-15 20:32:44 +0700

    Create viewLibrary.php

---
+++ app/Views/content/viewLibrary.php
@@ -0,0 +1,306 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SIMPERS | Sistem Informasi Manajemen Perpustakaan</title>
+    
+    <link href="https://fonts.googleapis.com/css?family=Quicksand:400,700&display=swap" rel="stylesheet">
+    
+    <link rel="stylesheet" href="/css/all.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+     
+    <link rel="stylesheet" href="/css/libs.min.css">
+    <link rel="stylesheet" href="/css/style.min.css">
+
+    <!-- Bootstrap Core CSS -->
+    <link rel="stylesheet" href="/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="/css/viewMasterAdmin.css">
+
+    <!-- Custom Fonts -->
+    <link rel="stylesheet" href="/css/font-awesome.min.css" type="text/css">
+    
+    
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+
+</head>
+<body class="page-mart">
+ 
+ <div class="wrapper">
+ <div class="content">
+
+<div class="newest-release">
+   
+    <div class="container">
+       
+        <div class="newest__products title">
+             <b>Library</b>        
+
+
+   </div>
+    
+    </div>
+</div> 
+                     
+<section class="products">
+    <div class="container">
+
+       <div class="products__inner">             
+        <div class="content-blocks">
+             <div class="content__block mix category-a category-new" data-order="1">
+                 <img class="content__block_img" src="" alt="">
+                 <a href="#" class="content__block-img">
+                 <button class="button-info" type="submit">Info</button>
+                 <button class="button-demo" type="submit">Pinjam</button>
+                </a>
+                 
+                 
+                 <div class="content__desc">
+                    
+                     <div class="content__desc-title">
+                         Buku 1    
+                     </div>
+                     
+                     <div class="content__desc-name">
+                        <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                         <div class="content__desc-theme">
+                             Penulis    
+                         </div>
+                     </div>
+                      
+                    <div class="content__desc-text">
+                         Deskripsi......   
+                    </div> 
+                    
+                    <div class="content__item">
+                       
+                        <a href="#" class="content__item-money">
+                            Kategori
+                        </a>                        
+                    </div>    
+                 </div> 
+             </div> 
+
+             <div class="content__block mix category-a category-new" data-order="2">
+                <img class="content__block_img" src="" alt="">
+                <a href="#" class="content__block-img">
+                <button class="button-info" type="submit">Info</button>
+                <button class="button-demo" type="submit">Pinjam</button>
+               </a>
+                
+                
+                <div class="content__desc">
+                   
+                    <div class="content__desc-title">
+                        Buku 2    
+                    </div>
+                    
+                    <div class="content__desc-name">
+                       <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                        <div class="content__desc-theme">
+                            Penulis    
+                        </div>
+                    </div>
+                     
+                   <div class="content__desc-text">
+                        Deskripsi......   
+                   </div> 
+                   
+                   <div class="content__item">
+                      
+                       <a href="#" class="content__item-money">
+                           Kategori
+                       </a>                        
+                   </div>    
+                </div> 
+            </div> 
+
+            <div class="content__block mix category-a category-new" data-order="3">
+                <img class="content__block_img" src="" alt="">
+                <a href="#" class="content__block-img">
+                <button class="button-info" type="submit">Info</button>
+                <button class="button-demo" type="submit">Pinjam</button>
+               </a>
+                
+                
+                <div class="content__desc">
+                   
+                    <div class="content__desc-title">
+                        Buku 3    
+                    </div>
+                    
+                    <div class="content__desc-name">
+                       <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                        <div class="content__desc-theme">
+                            Penulis    
+                        </div>
+                    </div>
+                     
+                   <div class="content__desc-text">
+                        Deskripsi......   
+                   </div> 
+                   
+                   <div class="content__item">
+                      
+                       <a href="#" class="content__item-money">
+                           Kategori
+                       </a>                        
+                   </div>    
+                </div> 
+            </div> 
+
+            <div class="content__block mix category-a category-new" data-order="4">
+                <img class="content__block_img" src="" alt="">
+                <a href="#" class="content__block-img">
+                <button class="button-info" type="submit">Info</button>
+                <button class="button-demo" type="submit">Pinjam</button>
+               </a>
+                
+                
+                <div class="content__desc">
+                   
+                    <div class="content__desc-title">
+                        Buku 4    
+                    </div>
+                    
+                    <div class="content__desc-name">
+                       <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                        <div class="content__desc-theme">
+                            Penulis    
+                        </div>
+                    </div>
+                     
+                   <div class="content__desc-text">
+                        Deskripsi......   
+                   </div> 
+                   
+                   <div class="content__item">
+                      
+                       <a href="#" class="content__item-money">
+                           Kategori
+                       </a>                        
+                   </div>    
+                </div> 
+            </div> 
+
+            <div class="content__block mix category-a category-new" data-order="5">
+                <img class="content__block_img" src="" alt="">
+                <a href="#" class="content__block-img">
+                <button class="button-info" type="submit">Info</button>
+                <button class="button-demo" type="submit">Pinjam</button>
+               </a>
+                
+                
+                <div class="content__desc">
+                   
+                    <div class="content__desc-title">
+                        Buku 5    
+                    </div>
+                    
+                    <div class="content__desc-name">
+                       <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                        <div class="content__desc-theme">
+                            Penulis    
+                        </div>
+                    </div>
+                     
+                   <div class="content__desc-text">
+                        Deskripsi......   
+                   </div> 
+                   
+                   <div class="content__item">
+                      
+                       <a href="#" class="content__item-money">
+                           Kategori
+                       </a>                        
+                   </div>    
+                </div> 
+            </div> 
+
+            <div class="content__block mix category-a category-new" data-order="6">
+                <img class="content__block_img" src="" alt="">
+                <a href="#" class="content__block-img">
+                <button class="button-info" type="submit">Info</button>
+                <button class="button-demo" type="submit">Pinjam</button>
+               </a>
+                
+                
+                <div class="content__desc">
+                   
+                    <div class="content__desc-title">
+                        Buku 6
+                    </div>
+                    
+                    <div class="content__desc-name">
+                       <a href="#"><img class="content__desc-circle" src="img/weekly/user-Ellipse.png" alt="circle"></a>
+                        <div class="content__desc-theme">
+                            Penulis    
+                        </div>
+                    </div>
+                     
+                   <div class="content__desc-text">
+                        Deskripsi......   
+                   </div> 
+                   
+                   <div class="content__item">
+                      
+                       <a href="#" class="content__item-money">
+                           Kategori
+                       </a>                        
+                   </div>    
+                </div> 
+            </div> 
+          
+        </div>  
+      </div>
+        
+    </div>
+</section>    
+    
+<footer class="footer">
+   <div class="footer__content">
+    <div class="container">
+    
+     <div class="footer__inner">
+        
+         <div class="footer__col footer__col-adress">
+             <div class="footer__col-logo">
+                <a class="navbar-brand" href="index.html"><i class="fa fa-bookmark fa-fw"></i> <b>SIMPERS</b></a>
+             </div>
+             <div class="footer__col-text">
+                SIMPERS - Sebuah platform yang memudahkan kegiatan perpustakaan anda.
+             </div> 
+               <div class="footer__info">
+                   <div class="footer__info-tel icon-phone footer__info-icon">
+                       Telepon: (0721) 701609
+                   </div>
+                   <div class="footer__info-mail icon-envelop footer__info-icon">
+                    library@kpa.unila.ac.id
+                   </div>
+                   <div class="footer__info-point icon-location footer__info-icon">
+                    Gedong Meneng, Kec. Rajabasa, 
+                    Kota Bandar Lampung, Lampung 35142
+                   </div>
+               </div>
+                
+         </div>        
+     </div>              
+    </div>
+      <div class="footer__copy">
+          <div class="footer__copy-text">
+              ©2022 <span>SIMPERS</span>-Sistem Informasi Manajemen Perpustakaan
+          </div>      
+      </div>   
+  </div>       
+</footer>      
+</div>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<script src="js/libs.min.js"></script>
+<script src="js/main.js"></script>
+
+<script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+</body>
+</html>
